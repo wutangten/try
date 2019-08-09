9f99801d5fb2873be097309f5d15d21545bb7e2a
wutangten <0brucederland1@gmail.com>
2019-08-09 13:37:37 -0700

    add cpp general format

---
+++ basicCppFormat.cpp
@@ -0,0 +1,20 @@
+/************************************************************************
+//
+//  Filename: .cpp
+//
+//  Code Description:
+//
+//  Created by brucederland on xx/xx/19.
+//
+************************************************************************/
+
+#include <iostream> /* basic input output*/
+
+using namespace std;
+
+int smallestIndex(int num[]);
+
+int main()
+{
+	/* Code */
+}
